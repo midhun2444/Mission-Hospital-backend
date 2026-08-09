b998cd14282af06f256161ef416f0e0c73e94580
midhun2444 <github2444@gmail.com>
2026-08-09 16:47:43 +0530

    Update import paths for auth and role middleware

--- doctor.routes.js
+++ doctor.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { listDoctors, getDoctor, createDoctor, updateDoctor } = require("../controllers/doctor.controller");
-const protect = require("../middleware/auth");
-const allowRoles = require("../middleware/role");
+const protect = require("./auth");
+const allowRoles = require("./role");
 
 router.get("/", listDoctors);
 router.get("/:id", getDoctor);
