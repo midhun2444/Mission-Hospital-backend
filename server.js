c6eecf198e849e3fbd7a96b62a594f615cfdd5bb
midhun2444 <github2444@gmail.com>
2026-08-09 16:15:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
-const connectDB = require("./config/db");
+const connectDB = require("./db");
 
 const authRoutes = require("./routes/auth.routes");
 const departmentRoutes = require("./routes/department.routes");
