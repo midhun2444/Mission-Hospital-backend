8d32d3b502fb33a92971e7b8298445de46f6dff7
midhun2444 <github2444@gmail.com>
2026-08-09 17:06:07 +0530

    Refactor connectDB function for clarity and error handling

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
-async function connectDB() {
+const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) throw new Error("MONGO_URI is not set in .env");
-    await mongoose.connect(uri);
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not set");
+    }
+
+    await mongoose.connect(mongoURI);
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
