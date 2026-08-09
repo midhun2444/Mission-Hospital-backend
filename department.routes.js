9d0aa2792c75e4d8486cfefe0e67b32a52d6fc59
midhun2444 <github2444@gmail.com>
2026-08-09 16:46:12 +0530

    Update import paths for auth and role middleware

--- department.routes.js
+++ department.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { listDepartments, createDepartment } = require("./department.controller");
-const protect = require("../middleware/auth");
-const allowRoles = require("../middleware/role");
+const protect = require("./auth");
+const allowRoles = require("./role");
 
 router.get("/", listDepartments);
 router.post("/", protect, allowRoles("admin"), createDepartment);
