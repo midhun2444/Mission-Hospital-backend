fe41b2d9a99c3f957d4db376058dcdd35f4de253
midhun2444 <github2444@gmail.com>
2026-08-09 16:40:14 +0530

    Update User model import path in auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("./User");
 
 // Verifies the JWT in the Authorization header and attaches req.user
 async function protect(req, res, next) {
