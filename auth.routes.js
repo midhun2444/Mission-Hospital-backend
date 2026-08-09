4d1d0dd94b6ecc47229a181fd6cf672ddf4f7d22
midhun2444 <github2444@gmail.com>
2026-08-09 16:38:58 +0530

    Update auth.routes.js

--- auth.routes.js
+++ auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { register, login, me } = require("./auth.controller");
-const protect = require("../middleware/auth");
+const protect = require("./auth");
 
 router.post("/register", register);
 router.post("/login", login);
